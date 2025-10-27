485e7576388b70ede597c5ce00c132f964a27e67
lucasilvaO <oliveira.lucasilva20@gmail.com>
2025-10-27 16:51:34 -0300

    fix: Formatacao Black corrigida

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,20 @@
 import pytest
-from starlette.testclient import TestClient 
+from starlette.testclient import TestClient
 from app.main import app, db, Student
 
 
 @pytest.fixture(autouse=True)
 def setup_db():
-    
-    initial_db = [
-        Student(id=1, name="Lucas", email="lucas@gmail.com")
-    ]
+
+    initial_db = [Student(id=1, name="Lucas", email="lucas@gmail.com")]
     db[:] = initial_db
     yield
 
 
 @pytest.fixture
 def client():
-    
-    with TestClient(app) as client: 
+
+    with TestClient(app) as client:
         yield client
 
 
@@ -33,7 +31,7 @@ def test_get_student_success(client: TestClient):
     response = client.get("/students/1")
     assert response.status_code == 200
     assert response.json()["name"] == "Lucas"
-    
+
 
 def test_get_student_failure(client: TestClient):
     """Teste de falha esperada: ID não encontrado (404)."""
@@ -47,13 +45,13 @@ def test_create_student_success(client: TestClient):
     new_student_data = {"name": "Novo Aluno", "email": "novo.aluno@exemplo.com"}
     response = client.post("/students", json=new_student_data)
     assert response.status_code == 201
-    
-    assert response.json()["id"] == 2 
+
+    assert response.json()["id"] == 2
 
 
 def test_create_student_failure_duplicate_email(client: TestClient):
     """Teste de falha esperada: Tenta usar o e-mail duplicado 'lucas@gmail.com'."""
-    duplicate_student_data = {"name": "Aluno Duplicado", "email": "lucas@gmail.com"} 
+    duplicate_student_data = {"name": "Aluno Duplicado", "email": "lucas@gmail.com"}
     response = client.post("/students", json=duplicate_student_data)
     assert response.status_code == 400
     assert response.json()["detail"] == "Email already registered"
